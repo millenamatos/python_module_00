f69f45b2af3283063c9c87d45fbee9e6db89cecf
Millena Matos Dos Santos <mmatos-d@c1r2p3.42sp.org.br>
2026-09-14 13:20:32 -0300

    module 00

--- ex6/ft_count_harvest_recursive.py
+++ ex6/ft_count_harvest_recursive.py
@@ -1,8 +1,9 @@
 def ft_recursive_util(current_day, harvest_day):
     if current_day <= harvest_day:
         print("Day", current_day)
-        current_day +=  1
+        current_day += 1
         ft_recursive_util(current_day, harvest_day)
+
 
 def ft_count_harvest_recursive():
     harvest_day = int(input("Days until harvest: "))
